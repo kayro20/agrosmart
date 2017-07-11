4d704252bddec6a24b791a384a34afc538915051
Flávia Fernanda Rocha Moraes <flaviafernanda.moraes@gmail.com>
2017-07-10 22:50:30 -0300

    Adding treeview menu sidebar

--- scripts/components/sidebar.jsx
+++ scripts/components/sidebar.jsx
@@ -1,14 +1,69 @@
 import React from 'react';
 
+let farms = [
+  "Algar", "Amanda", "Banco Alfa", "Coca Cola",
+  "Estufa Esalq", "Figueira Branca II", "Hernandes",
+  "Jóia", "Manoel Viana", "Modelo 2", "Monsanto",
+  "NDJ Israel - Avocado", "Palmeira", "Raízen",
+  "Restinga", "Rincão", "Santa Colomba", "Soledad",
+  "Sto Antônio da Estiva", "Terra Nova"
+];
+
+class SubmenuItem extends React.Component {
+  render() {
+    return <div>
+      <a href={ this.props.route }>{ this.props.children }</a>
+    </div>
+  }
+}
+
 class SidebarMenuItem extends React.Component {
+  onClick(event) {
+    if (this.props.children) {
+      event.preventDefault();
+      this.setState({open: !this.state.open});
+    }
+  }
+
+  getArrow() {
+    if (!this.props.children) {
+      return null;
+    }
+
+    if (this.state.open) {
+      return <i className="chevron fa fa-chevron-down"/>;
+    }
+
+    return <i className="chevron fa fa-chevron-left"/>;
+  }
+
+  getSubmenu() {
+    if (!this.props.children) {
+      return null;
+    }
+
+    return <div className={"sidebar-submenu " + (this.state.open ? 'active' : '')}>
+      { this.props.children }
+    </div>
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = {open: false};
+  }
+
   render() {
     return (
-      <a className="sidebar-menu-item" href={this.props.route}>
-        <i className={this.props.icon}/>
-        <div className="item-content">
-          { this.props.text }
+      <div className="sidebar-menu-item" href={this.props.route}>
+        <div className="sidebar-main" onClick={this.onClick.bind(this)}>
+          <i className={this.props.icon}/>
+          <div className="item-text">
+            { this.props.text }
+          </div>
+          { this.getArrow() }
         </div>
-      </a>
+        { this.getSubmenu() }
+      </div>
     );
   }
 }
@@ -56,7 +111,15 @@ export default class Sidebar extends React.Component {
           </div>
         </div>
         <div className="sidebar-menu">
-          <SidebarMenuItem icon="fa fa-th-large" text="Fazendas"/>
+          <SidebarMenuItem icon="fa fa-th-large" text="Fazendas">
+            {farms.map((farm, index) => {
+              return (
+                <SubmenuItem route={'http://cultivointeligente.com.br/farms/' + index} key={index}>
+                  { farm }
+                </SubmenuItem>
+              );
+            })}
+          </SidebarMenuItem>
           <SidebarMenuItem icon="fa fa-dashboard" text="Dashboard Banco Alfa" route="http://cultivointeligente.com.br/farms/19"/>
           <SidebarMenuItem icon="fa fa-list-alt" text="Histórico" route="http://cultivointeligente.com.br/historical"/>
           <SidebarMenuItem icon="fa fa-sign-out" text="Sair" route="http://cultivointeligente.com.br/users/sign_out"/>
