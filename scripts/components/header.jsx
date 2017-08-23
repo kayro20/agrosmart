ab80e3466b101eb5aea0fed276d0707d85d28157
Flávia Fernanda Rocha Moraes <flaviafernanda.moraes@gmail.com>
2017-08-23 15:38:30 -0300

    Initial commit SC test

--- scripts/components/header.jsx
+++ scripts/components/header.jsx
@@ -1,29 +1,126 @@
 import React from 'react';
+import styled, { ThemeProvider } from 'styled-components';
+import { Button as ReactstrapButton } from 'reactstrap';
+import { Navbar } from 'reactstrap';
+
+const DropdownButton = styled(ReactstrapButton)`
+${(props) => `
+  &.btn {
+    border: 1px solid ${props.theme.$color};
+    float: left;
+    background-color: ${props.theme.$color};
+  }
+`}
+`
+
+const NavbarTop = styled.nav`
+  background: white;
+  position: fixed;
+
+  .header-wrapper {
+    background: white;
+    display: table;
+    width: 100%;
+    border-bottom: solid 1px rgba(0, 0, 0, 0.08);
+  }
+
+  .header {
+    text-align: center;
+    height: 61px;
+    vertical-align: middle;
+    display: table-cell;
+    padding: 0 10px;
+
+    .icon-btn {
+      padding: 10px;
+    }
+
+    .agromenu {
+      margin: 10px;
+    }
+
+    button {
+      padding: 5px 13px;
+      border: none;
+      border-radius: 3px;
+      color: white;
+    }
+
+    .btn-notification {
+      float: right;
+      position: relative;
+      button {
+        background: white;
+        i {
+          color: #b3b3b3;
+          font-size: 18px;
+        }
+        span {
+          background: rgba(248, 172, 89, 0.78);
+          padding: 0 5px;
+          font-size: 12px;
+          border-radius: 2px;
+          position: absolute;
+          top: 5px;
+          right: 12px;
+        }
+      }
+    }
+  }
+
+  .dropdown-menu-right li a {
+    font-size: 13px;
+    font-weight: 600;
+
+    i {
+      padding-right: 5px;
+      color: #2cb971;
+    }
+  }
+
+`;
 
 export default class Header extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      toggle: false,
+      color: 'white',
+    }
+    this.toggleState = this.toggleState.bind(this);
+  }
+
+  toggleState() {
+    var custom = document.getElementById('color').value;
+    this.setState({ toggle: !this.state.toggle, color: custom });
+  }
+
   render() {
+    console.log(this.props);
     return(
-      <nav className="navbar-fixed-top">
-        <div className="header-wrapper">
-          <div className="header">
-            <div className="btn-menu agromenu">
-              <button><i className="fa fa-bars"/></button>
-            </div>
-            <img src={require('../../assets/images/logo-navbar.png')} alt="Logo verde"/>
-            <div className="icon-btn btn-notification">
-              <button className="dropdown-toggle" data-toggle="dropdown"><i className="fa fa-envelope"/><span className="indicator">10</span></button>
-              <ul className="dropdown-menu dropdown-menu-right">
-                <li>
-                  <a href="http://cultivointeligente.com.br/inbox/mark_as_read">
-                    <i className="fa fa-envelope-open"></i>
-                    Marcar todas como lidas
-                  </a>
-                </li>
-              </ul>
+      <div>
+        <NavbarTop className="navbar-fixed-top">
+          <div className="header-wrapper">
+            <div className="header">
+              <div className="btn-menu agromenu">
+                <DropdownButton color={this.state.color}><i className="fa fa-bars"/></DropdownButton>
+              </div>
+              <img src={require('../../assets/images/logo-navbar.png')} alt="Logo verde"/>
+              <div className="icon-btn btn-notification">
+                <button className="dropdown-toggle" data-toggle="dropdown"><i className="fa fa-envelope"/><span className="indicator">10</span></button>
+                <ul className="dropdown-menu dropdown-menu-right">
+                  <li>
+                    <a href="http://cultivointeligente.com.br/inbox/mark_as_read">
+                      <i className="fa fa-envelope-open"></i>
+                      Marcar todas como lidas
+                    </a>
+                  </li>
+                </ul>
+              </div>
             </div>
           </div>
-        </div>
-      </nav>
+        </NavbarTop>
+      </div>
     );
   }
 }
--- scripts/views/index.jsx
+++ scripts/views/index.jsx
@@ -1,146 +1,63 @@
 import React from 'react';
+import styled, { ThemeProvider } from 'styled-components';
+import { Button as ReactstrapButton } from 'reactstrap';
 import Header from 'components/header';
-import Summary from 'components/summary';
-import InfoCard from 'components/info-card';
-import WeatherForecast from 'components/weather-forecast';
-import Sidebar from 'components/sidebar';
-import Footer from 'components/footer';
 
-var data_one = {
-  labels: ['09:00', '14:00', '19:00', '00:00', '04:00'],
-  series: [[15, 22, 20, 14, 13]]
-}
-var data_two = {
-  labels: ['09:00', '14:00', '19:00', '00:00', '04:00'],
-  series: [[18, 25, 23, 15, 14]]
-}
-var data_three = {
-  labels: ['09:00', '14:00', '19:00', '00:00', '04:00'],
-  series: [[16, 23, 20, 15, 15]]
-}
+const Button = styled(ReactstrapButton)`
+${(props) => `
+  &.btn {
+    border: 1px solid purple;
+
+    &.btn-success {
+      background-color: ${props.theme.$color};
+    }
+  }
+`}
+`
+
+// Create a <Title> react component that renders an <h1> which is centered, palevioletred and sized at 1.5em
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: ${props => props.color || "red"};
+  &.light { font-weight: inherit; }
+  &.bold { font-weight: 600; }
+`;
+
+// Create a <Wrapper> react component that renders a <section> with some padding and a papayawhip background
+const Wrapper = styled.section`
+  padding: 4em;
+  background: papayawhip;
+`;
 
 export default class Dashboard extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      toggle: false,
+      color: "white",
+      theme: {'$color': 'purple'},
+    }
+    this.toggleState = this.toggleState.bind(this);
+  }
+
+  toggleState() {
+    var obj = {'abc': 'ed'};
+    console.log(obj['abc']);
+    var custom = document.getElementById('color').value;
+    this.setState({ toggle: !this.state.toggle, color: custom, theme: {'$color': custom} });
+  }
+
   render() {
     return (
-    <div>
-      <Header />
-      <div className="client-name">
-        <h2>Fazenda Banco Alfa</h2>
-      </div>
-      <div className="wraper-content">
-        <Sidebar />
-        <div className="container">
-          <Summary>
-            <Summary.MultiCard
-              topIcon="wi wi-thermometer fa-4x" topText="Temperatura" topValue="21,09 ºC"
-              bottomIcon="wi wi-humidity fa-4x" bottomText="Umidade" bottomValue="79%"
-            />
-            <Summary.MultiCard
-              topIcon="fa fa-leaf fa-4x" topText="Ponto de Orvalho" topValue="17,11 ºC"
-              bottomIcon="wi wi-showers fa-4x" bottomText="Chuva" bottomValue="0,0mm"
-            />
-            <Summary.SingleCard
-              icon="wi wi-wind towards-128-deg fa-4x" text="Velocidade" value="10,9km/h"
-              iconText="sudeste"
-            />
-            <Summary.SingleCard
-              icon="wi wi-day-sunny fa-4x m-b-sm" text="Alta Luminosidade" value="540,9 W/m²"
-              center={true}
-            />
-          </Summary>
-          <WeatherForecast>
-            <WeatherForecast.WeatherCard
-              title="Segunda-feira"
-              iconDay="wi wi-day-cloudy"
-              min="13ºC"
-              average="22ºC"
-              max="28ºC"
-              rainIconLeft="wi wi-umbrella"
-              rainIconRight="wi wi-raindrop"
-              chance="55%"
-              quantity="2,0mm"
-              graphData = {data_one}
-            />
-            <WeatherForecast.WeatherCard
-              title="Terça-feira"
-              iconDay="wi wi-day-sunny"
-              min="15ºC"
-              average="26ºC"
-              max="30ºC"
-              rainIconLeft="wi wi-umbrella"
-              rainIconRight="wi wi-raindrop"
-              chance="5%"
-              quantity="0,0mm"
-              graphData = {data_two}
-            />
-            <WeatherForecast.WeatherCard
-              title="Quarta-feira"
-              iconDay="wi wi-day-rain"
-              min="14ºC"
-              average="23ºC"
-              max="27ºC"
-              rainIconLeft="wi wi-umbrella"
-              rainIconRight="wi wi-raindrop"
-              chance="80%"
-              quantity="2,5mm "
-              graphData = {data_three}
-            />
-          </WeatherForecast>
-          <InfoCard>
-            <InfoCard.Pivo
-              title="Pivo 1"
-              background="https://storage.googleapis.com/vai-agrosmart/uploads/field/image/image/215/thumb_Pivo1.png"
-              culture="Milho A."
-              phase="Produção"
-              status="Não irrigar"
-            />
-            <InfoCard.Pivo
-              title="Pivo 2"
-              background="https://storage.googleapis.com/vai-agrosmart/uploads/field/image/image/216/thumb_Pivo2.png"
-              culture="Milho B."
-              phase="Produção"
-              status="Irrigar"
-            />
-            <InfoCard.Pivo
-              title="Pivo 3"
-              background="https://storage.googleapis.com/vai-agrosmart/uploads/field/image/image/217/thumb_Pivo3.png"
-              culture="Milho C."
-              phase="Cultivo"
-              status="Não irrigar"
-            />
-            <InfoCard.Pivo
-              title="Pivo 4"
-              background="https://storage.googleapis.com/vai-agrosmart/uploads/field/image/image/218/thumb_Pivo4.png"
-              culture="Milho D."
-              phase="Produção"
-              status="Não irrigar"
-            />
-            <InfoCard.Pivo
-              title="Pivo 5"
-              background="https://storage.googleapis.com/vai-agrosmart/uploads/field/image/image/219/thumb_Pivo5.png"
-              culture="Milho A."
-              phase="Crescimento"
-              status="Não irrigar"
-            />
-            <InfoCard.Pivo
-              title="Pivo 6"
-              background="https://storage.googleapis.com/vai-agrosmart/uploads/field/image/image/220/thumb_Pivo6.png"
-              culture="Milho A."
-              phase="Produção"
-              status="Não irrigar"
-            />
-            <InfoCard.Pivo
-              title="Pivo 7"
-              background="https://storage.googleapis.com/vai-agrosmart/uploads/field/image/image/221/thumb_Pivo7.png"
-              culture="Milho A."
-              phase="Produção"
-              status="Não irrigar"
-            />
-          </InfoCard>
-        </div>
-      </div>
-      <Footer />
-    </div>
+        <ThemeProvider theme={this.state.theme}>
+        <Wrapper>
+          <Header theme={this.state.theme}/>
+          <Title color={this.state.color} className={this.state.toggle ? "light" : "bold"}>Hello World, this is my first styled component!</Title>
+          <div><input id="color" placeholder="color here"></input></div>
+          <Button className="dropdown-toggle" color="success" onClick={this.toggleState}>Toggle typography {this.state.toggle ? "heavy" : "light"}</Button>
+        </Wrapper>
+      </ThemeProvider>
     );
   }
 }
